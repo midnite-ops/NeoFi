6a820c4e5d9877061628c5acb34765e18e9b91f9
cakezero <sareahj.com@gmail.com>
2025-08-18 00:55:11 +0100

    Finished the hero seciton of the project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import neofi from '../assets/hero/neo-logo.svg';
+import play from '../assets/hero/play.svg';
+import heroImg from '../assets/hero/hero-right-image.svg'
+function Hero(){
+    const [active, setActive] = useState('Home')
+    function handleClick(value){
+        setActive(value)
+    }
+    const navLinks = ['Home', 'Features', 'Testimonial', 'FAQ']
+    return(
+        <section className='flex flex-col h-screen'>
+            <nav className="flex justify-between items-center" id='navbar'>
+                <div className='flex space-x-5 items-center'>
+                    <img src={neofi} className='w-[32px]' alt="a neofi logo" />
+                    <h1 className='font-medium text-[22px]'>NeoFi</h1>
+                </div>
+                <div>
+                    <ul className='flex space-x-10 items-center'>
+                        {navLinks.map(item => (
+                            <li className={`font-alex text-gray-500 hover:cursor-pointer ${active === item ? 'text-white' : ''}`} onClick={() => {handleClick(item)}}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='flex space-x-5 items-center'>
+                    <p>Log in</p>
+                    <button>Try For Free</button>
+                </div>
+            </nav>
+
+            <section className="flex-col flex lg:flex-row justify-between flex-1 items-center" id='hero'>
+                <div className='flex-2 w-full flex space-y-10 flex-col'>
+                    <div className='flex flex-col space-y-6'>
+                        <div className='border py-2.5 px-3 rounded-3xl flex items-center space-x-2 w-fit'>
+                            <div className='rounded-full h-2 w-2 bg-green'></div>
+                            <p className='text-green text-sm'>Decentralized</p>
+                        </div>
+                        <h1 className='lg:w-[661px]'>Revolutionizing<br/> Web3 Finance</h1>
+                        <p className='text-start font-inter text-[20px] font-light text-[#999999] w-[583px]'>
+                            Secure, scalable and decentralized solutions for your digital assets-experience the future of digital freedom.
+                        </p>
+                    </div>
+                    
+                    <div className='flex space-x-5 w-fit'>
+                        <button className='py-3.5 px-6'>Get Started</button>
+                        <button className='py-3.5 px-6 bg-transparent border border-white text-white flex'>
+                            <img src={play} className='mr-1.5' alt="a play icon" />
+                            Watch Video
+                        </button>
+                    </div>
+                </div>
+                <div className='flex-1'>
+                    <img src={heroImg} className='w-full' alt="" />
+                </div>
+            </section>
+        </section>
+        
+    )
+}
+
+export default Hero
---
+++ src/components/Partners.jsx
@@ -0,0 +1,43 @@
+import logo1 from '../assets/partners/1.svg';
+import logo2 from '../assets/partners/2.svg';
+import logo3 from '../assets/partners/3.svg';
+import logo4 from '../assets/partners/4.svg';
+function Partners(){
+    const partners = [
+        {
+            title: 'Logoipsum',
+            img: logo1
+        },
+        {
+            title: 'Logoipsum',
+            img: logo2
+        },
+        {
+            title: 'Logoipsum',
+            img: logo3
+        },
+        {
+            title: 'Logoipsum',
+            img: logo4
+        }
+    ]
+    return(
+        <section id="partners" className="bg-gray py-10 -mx-[114px] flex flex-col items-center space-y-10">
+            <div>
+                <p>
+                    Satisfied Partners
+                </p>
+            </div>
+            <div className='flex space-x-[72px] flex-wrap items-center'>
+                {partners.map((partner, index) => (
+                    <div key={index} className='flex items-center space-x-2'>
+                        <img src={partner.img} alt="logo" />
+                        <p>{partner.title}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Partners
